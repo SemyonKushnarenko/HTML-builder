1b517dea634849412a66de943190942532edbd9d
Trisemya <trisemya@gmail.com>
2022-05-14 16:49:45 +0300

    feat: add 3rd task decision

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs/promises');
+async function findFiles(searchPath) {
+    let files = await fsPromises.readdir(path.join(__dirname, ...searchPath), { withFileTypes: true });
+    let nFiles = [];
+    for (let file of files) {
+        if (file.isFile()) {
+            file.path = searchPath;
+            nFiles.push(file);
+        }
+        else if (file.isDirectory()) {
+            let newSearchPath = [...searchPath];
+            newSearchPath.push(file.name);
+            let newFiles = await findFiles(newSearchPath);
+            nFiles = [...nFiles, ...newFiles];
+        }
+    }
+    return nFiles;
+}
+async function getStats() {
+    let files = await findFiles(['secret-folder']);
+    for (let file of files) {
+        fs.stat(path.join(__dirname, ...file.path, file.name), (err, stats) => {
+            console.log(path.join(__dirname, ...file.path));
+            if (err) {
+                throw err;
+            }
+            const name = file.name.split('.')[0] === '' ? file.name : file.name.split('.').slice(0, -1).join('.');
+            const extension = path.extname(path.join(__dirname, ...file.path, file.name));
+            console.log(name + ' - ', extension.split('').slice(1).join('') + ' - ', stats.size / 1024 + 'KB');
+        });
+    }
+}
+getStats();
